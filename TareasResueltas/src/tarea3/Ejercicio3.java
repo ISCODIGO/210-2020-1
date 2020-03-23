ef4d9ff7a0d2a695b7cf02b553f09729d058ee96
Jose Enrique Avila <senquevila@gmail.com>
2020-03-23 01:38:16 -0600

    Ejercicio 3

--- TareasResueltas/src/tarea3/Ejercicio3.java
+++ TareasResueltas/src/tarea3/Ejercicio3.java
@@ -5,15 +5,8 @@
  */
 package tarea3;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -22,115 +15,66 @@ import java.util.logging.Logger;
 public class Ejercicio3 {
 
     public static void main(String[] args) {
-        String boleto250 = "250";
-        String boleto100 = "100";
-        String boleto50 = "50";
-        String boleto25 = "25";
-
+        float[] precios = {250, 100, 50, 25};
+        int[] vendidos = new int[precios.length];
+        double[] ventas = new double[precios.length];
         Scanner entrada = new Scanner(System.in);
-        int vendidosA = 0;
-        int vendidosB = 0;
-        int vendidosC = 0;
-        int vendidosD = 0;
+        String opcion = "";
 
-        String opcion = null;
-        int importeVentasA = 0;
-        int importeVentasB = 0;
-        int importeVentasC = 0;
-        int importeVentasD = 0;
-        int importeTotal = 0;
         do {
-            System.out.println("Elija una de las siguientes opciones \n"
-                    + "a.- para ingresar el total de boletos de 250$ \n"
-                    + "b.- para ingresar el total de boletos de 100$ \n"
-                    + "c.- para ingresar el total de boletos de 50$ \n"
-                    + "d.- para ingresar el total de boletos de 25$ \n"
-                    + "e.- para salir del sistema.");
+            System.out.println("Elija una de las siguientes opciones\n"
+                    + "[1] Cantidad de boletos de $250\n"
+                    + "[2] Cantidad de boletos de $100\n"
+                    + "[3] Cantidad de boletos de $50\n"
+                    + "[4] Cantidad de boletos de $25\n"
+                    + "[0] Salir");
 
             opcion = entrada.nextLine();
 
             switch (opcion) {
-                case "a":
-                    System.out.println("Ingrese la cantidad de boletos vendidos de 250$ ");
-                    vendidosA = entrada.nextInt();
-                    importeVentasA = Integer.parseInt(boleto250) * vendidosA;
+                case "0":
+                    System.exit(0);
                     break;
-                case "b":
-                    System.out.println("Ingrese la cantidad de boletos vendidos de 100$ ");
-                    vendidosB = entrada.nextInt();
-                    importeVentasB = Integer.parseInt(boleto100) * vendidosB;
+                case "1":
+                    System.out.print("Ingrese la cantidad de boletos vendidos de $250: ");
+                    vendidos[0] = entrada.nextInt();
                     break;
-                case "c":
-                    System.out.println("Ingrese la cantidad de boletos vendidos de 50$ ");
-                    vendidosC = entrada.nextInt();
-                    importeVentasC = Integer.parseInt(boleto50) * vendidosC;
+                case "2":
+                    System.out.print("Ingrese la cantidad de boletos vendidos de $100: ");
+                    vendidos[1] = entrada.nextInt();
                     break;
-                case "d":
-                    System.out.println("Ingrese la cantidad de boletos vendidos de 25$ ");
-                    vendidosD = entrada.nextInt();
-                    importeVentasD = Integer.parseInt(boleto25) * vendidosD;
+                case "3":
+                    System.out.print("Ingrese la cantidad de boletos vendidos de $50: ");
+                    vendidos[2] = entrada.nextInt();
                     break;
-
+                case "4":
+                    System.out.print("Ingrese la cantidad de boletos vendidos de $25: ");
+                    vendidos[3] = entrada.nextInt();
+                    break;
                 default:
-                    System.out.println("introduzca una opcion valida");
-
+                    System.out.println("Introduzca una opcion valida");
             }
 
-        } while (!("e".equals(opcion)));
+        } while (true);
 
-        importeTotal += (importeVentasA + importeVentasB + importeVentasC + importeVentasD);
+        double sumaVendida = 0;
+        for (int i = 0; i < precios.length; i++) {
+            ventas[i] = vendidos[i] * precios[i];
+            sumaVendida += ventas[i];
+        }
 
         try {
-            PrintWriter pw = new PrintWriter("miArchivo.txt");
-            pw.printf("%5s", "valor del boleto");
-            pw.printf("%40s", "numero de tickets vendidos\n");
-            pw.printf("%6s %40d\n", boleto250, vendidosA);
-            pw.printf("%6s %40d\n", boleto100, vendidosB);
-            pw.printf("%6s %40d\n", boleto50, vendidosC);
-            pw.printf("%6s %40d\n", boleto25, vendidosD);
+            PrintWriter pw = new PrintWriter("ejercicio3.txt");
+            pw.printf("%5s%40s%n", "ticketPrice", "numberOfTicketsSold");
+            pw.printf("%5s%40d%n", precios[0], ventas[0]);
+            pw.printf("%5s%40d%n", precios[1], ventas[1]);
+            pw.printf("%5s%40d%n", precios[2], ventas[2]);
+            pw.printf("%5s%40d%n", precios[3], ventas[3]);
             pw.println("");
-            pw.println("El importe total de ventas es de: " + importeTotal);
+            pw.printf("total: %.2f%n", sumaVendida);
             pw.close();
         } catch (Exception err) {
             System.out.println(err.getMessage());
         }
-
-        leerArchivo("miArchivo.txt");
-
     }
-
-    public static void leerArchivo(String ruta) {
-        String texto;
-        /*File archivo = new File("miArchivo.txt");
-        if(!archivo.exists()){
-            try{
-                archivo.createNewFile();
-                System.out.println(archivo.getName()+" ha sido creado");
-            }catch(IOException ex){
-                ex.printStackTrace();
-            }
-        }*/
-        try {
-            FileReader leer = new FileReader(ruta);
-            BufferedReader br = new BufferedReader(leer);
-            System.out.println(br.readLine());
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-
-        }
-
-    }
-
-    /*public static void escrituraArchivo(String ruta){
-        String texto = "texto en el archivo";
-        
-        try{
-            PrintWriter pw = new PrintWriter(ruta);
-            pw.printf("%6s %40d\n", boleto25, vendidosD);
-            pw.close();
-        }catch(Exception err){
-            
-        }
-        
-    }*/
 }
