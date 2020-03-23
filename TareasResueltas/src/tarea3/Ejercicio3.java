eb35d65d5cd57658e0d93c1998ba5a01f84c2a69
José Enrique Ávila <jeavila@unah.edu.hn>
2020-03-23 02:17:09 -0600

    finalizado ejercicio1.3.3

--- TareasResueltas/src/tarea3/Ejercicio3.java
+++ TareasResueltas/src/tarea3/Ejercicio3.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
 public class Ejercicio3 {
 
     public static void main(String[] args) {
-        float[] precios = {250, 100, 50, 25};
+        double[] precios = {250, 100, 50, 25};
         int[] vendidos = new int[precios.length];
         double[] ventas = new double[precios.length];
         Scanner entrada = new Scanner(System.in);
@@ -29,11 +29,10 @@ public class Ejercicio3 {
                     + "[4] Cantidad de boletos de $25\n"
                     + "[0] Salir");
 
-            opcion = entrada.nextLine();
+            opcion = entrada.next();
 
             switch (opcion) {
                 case "0":
-                    System.exit(0);
                     break;
                 case "1":
                     System.out.print("Ingrese la cantidad de boletos vendidos de $250: ");
@@ -54,8 +53,7 @@ public class Ejercicio3 {
                 default:
                     System.out.println("Introduzca una opcion valida");
             }
-
-        } while (true);
+        } while (!opcion.equals("0"));
 
         double sumaVendida = 0;
         for (int i = 0; i < precios.length; i++) {
@@ -65,16 +63,18 @@ public class Ejercicio3 {
 
         try {
             PrintWriter pw = new PrintWriter("ejercicio3.txt");
-            pw.printf("%5s%40s%n", "ticketPrice", "numberOfTicketsSold");
-            pw.printf("%5s%40d%n", precios[0], ventas[0]);
-            pw.printf("%5s%40d%n", precios[1], ventas[1]);
-            pw.printf("%5s%40d%n", precios[2], ventas[2]);
-            pw.printf("%5s%40d%n", precios[3], ventas[3]);
+            pw.printf("%5s%20s%n", "ticketPrice", "numberOfTicketsSold");
+            pw.printf("%7.2f%20.2f%n", precios[0], ventas[0]);
+            pw.printf("%7.2f%20.2f%n", precios[1], ventas[1]);
+            pw.printf("%7.2f%20.2f%n", precios[2], ventas[2]);
+            pw.printf("%7.2f%20.2f%n", precios[3], ventas[3]);
             pw.println("");
             pw.printf("total: %.2f%n", sumaVendida);
             pw.close();
         } catch (Exception err) {
             System.out.println(err.getMessage());
         }
+        
+        System.out.printf("Total de la venta es %.2f%n", sumaVendida);
     }
 }
